8943f03b8956eac66a16b666b9ea43b3dd569598
Angelo Laus <angelolausofcl@gmail.com>
2023-06-06 20:06:16 +0800

    feat: add login and register

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, session
+from register import register_user
+from login import login_user
 
 app = Flask(__name__, static_folder='static')
+app.secret_key = 'BigBrewsIsAwesome'
 
 
 @app.route('/')
@@ -11,6 +14,39 @@ def index():
 @app.route('/landing')
 def landing():
     return render_template('landing.html')
+
+
+@app.route('/register', methods=['POST'])
+def register_route():
+    # Get the form data
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    email_address = request.form['email_address']
+    password = request.form['password']
+
+    # Call the register_user function from register.py
+    result = register_user(first_name, last_name, email_address, password)
+
+    # Process the result and return a response
+    if result:
+        return 'Registration successful'
+    else:
+        return 'Registration failed'
+
+
+@app.route('/login', methods=['POST'])
+def login_route():
+    email_address = request.form['email_address']
+    password = request.form['password']
+
+    # Call the login_user function from login.py
+    result = login_user(email_address, password)
+
+    if result:
+        session['email_address'] = email_address
+        return ('It worked!')  # redirect('/selection')
+    else:
+        return 'Invalid email/password combination'
 
 
 if __name__ == '__main__':
