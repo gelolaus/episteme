65c39f4a13f6f3030052b20b031bd0012ed444fb
Angelo Laus <angelolausofcl@gmail.com>
2023-06-06 20:05:51 +0800

    feat: add login function

---
+++ login.py
@@ -0,0 +1,40 @@
+import mysql.connector
+from mysql.connector import Error
+
+
+def create_connection():
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="BigBrews-23",
+            auth_plugin="mysql_native_password",
+            database="episteme_db"
+        )
+        print("Connected to MySQL database")
+    except Error as e:
+        print(f"The error '{e}' occurred while connecting to MySQL database")
+    return connection
+
+
+def login_user(email_address, password):
+    db_connection = create_connection()
+    cursor = db_connection.cursor()
+
+    try:
+        query = "SELECT * FROM user WHERE email_address = %s AND password = %s"
+        values = (email_address, password)
+        cursor.execute(query, values)
+        user = cursor.fetchone()
+
+        if user:
+            return True
+        else:
+            return False
+    except Error as e:
+        print(f"The error '{e}' occurred while executing the SQL query")
+        return False
+    finally:
+        cursor.close()
+        db_connection.close()
