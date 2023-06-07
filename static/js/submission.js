b0950ca13166a395a910a6c772a7245375d9082a
Angelo Laus <angelolausofcl@gmail.com>
2023-06-08 07:16:57 +0800

    feat: add javascript for submission

---
+++ static/js/submission.js
@@ -0,0 +1,125 @@
+// Function to handle file input change event
+const fileInput = document.querySelector('#file-js-example input[type=file]');
+fileInput.onchange = () => {
+    if (fileInput.files.length > 0) {
+        const fileName = document.querySelector('#file-js-example .file-name');
+        fileName.textContent = fileInput.files[0].name;
+    }
+};
+
+// Function to handle radio button change event
+function handleRadioChange() {
+    const individualNameField = document.getElementById('individualNameField');
+    const groupNameField = document.getElementById('groupNameField');
+    const memberFields = document.getElementById('memberFields');
+
+    const individualRadio = document.querySelector('input[value="individual"]');
+    const groupRadio = document.querySelector('input[value="group"]');
+
+    if (individualRadio.checked) {
+        individualNameField.style.display = 'block';
+        groupNameField.style.display = 'none';
+        memberFields.style.display = 'none';
+    } else if (groupRadio.checked) {
+        individualNameField.style.display = 'none';
+        groupNameField.style.display = 'block';
+        memberFields.style.display = 'block';
+    }
+}
+
+// Get the keywords input element
+const keywordsInput = document.getElementById('keywords-input');
+
+// Get the tag container element
+const tagContainer = document.getElementById('tag-container');
+
+// Function to handle adding a tag
+function addTag() {
+    // Get the keywords text from the input
+    const keywordsText = keywordsInput.value.trim();
+
+    // Check if the input is not empty
+    if (keywordsText !== '') {
+        // Create a new tag element
+        const tagElement = document.createElement('span');
+        tagElement.classList.add('tag');
+        tagElement.textContent = keywordsText;
+
+        // Append the tag element to the tag container
+        tagContainer.appendChild(tagElement);
+
+        // Clear the input
+        keywordsInput.value = '';
+    }
+}
+
+
+// Add an event listener for the Enter key press on the keywords input
+keywordsInput.addEventListener('keypress', function (event) {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        addTag();
+    }
+});
+
+// Add an event listener for the click event on the tag container
+tagContainer.addEventListener('click', function (event) {
+    if (event.target.classList.contains('tag')) {
+        // Remove the tag element
+        event.target.remove();
+    }
+});
+
+// Function to prevent form submission on Enter key press
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.querySelector('form');
+
+    form.addEventListener('keydown', function (event) {
+        if (event.key === 'Enter') {
+            event.preventDefault();
+        }
+    });
+});
+
+// Function to handle form reset button click event
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.querySelector('form');
+    const clearFormButton = document.querySelector('#clearFormButton');
+
+    clearFormButton.addEventListener('click', function (event) {
+        event.preventDefault();
+        form.reset();
+
+        // Clear the keywords
+        const tagContainer = document.getElementById('tag-container');
+        tagContainer.innerHTML = '';
+
+        // Reset radio buttons to "Individual" checked
+        const individualRadio = document.querySelector('input[value="individual"]');
+        individualRadio.checked = true;
+
+        // Handle radio change to show/hide fields accordingly
+        handleRadioChange();
+    });
+});
+
+function submitForm(event) {
+    event.preventDefault();
+
+    // Get the form element
+    const form = document.querySelector('form');
+
+    // Create a new FormData object
+    const formData = new FormData(form);
+
+    // Get the keywords tags from the tag container
+    const tags = Array.from(tagContainer.querySelectorAll('.tag')).map(tag => tag.textContent);
+
+    // Add the tags to the formData object
+    formData.append('keywords', tags.join(', '));
+
+    // Make an AJAX request to submit the form data to the server
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', '/submit');
+    xhr.send(formData);
+}
