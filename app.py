7b6fcb5a0e52a9a70b3e5bb5ffd4747d48565611
Angelo Laus <angelolausofcl@gmail.com>
2023-06-10 19:22:56 +0800

    feat: multiple route changes

--- app.py
+++ app.py
@@ -1,24 +1,52 @@
-from flask import Flask, render_template, request, redirect, session, jsonify, render_template_string
+from flask import Flask, render_template, request, redirect, session, jsonify
 from register import register_user
 from login import login_user
 from admin import fetch_submissions
-from update_status import update_submission_status
+from personal import get_user_submissions, get_user_id
 import submit
-
 
 app = Flask(__name__, static_folder='static')
 app.secret_key = 'BigBrewsIsAwesome'
 
+# Define a list of routes that require authentication
+authenticated_routes = ['/homepage', '/submission', '/admin', '/update_status']
+
+
+@app.before_request
+def check_authentication():
+    # Check if the requested route requires authentication
+    if request.path in authenticated_routes and 'email_address' not in session:
+        # User is not logged in, redirect to landing
+        return redirect('/landing')
+
+    # Check if the user is trying to access the /admin page
+    if request.path == '/admin' and 'email_address' in session:
+        allowed_email_addresses = [
+            'dev@bigbrews'  # Modify this with the actual allowed email addresses
+        ]
+        if session['email_address'] not in allowed_email_addresses:
+            return redirect('/')
+
 
 @app.route('/')
 def index():
-    return 'Hello, world!'
+    if 'email_address' in session:
+        # User is logged in, redirect to homepage
+        return redirect('/homepage')
+    else:
+        # User is not logged in, redirect to landing
+        return redirect('/landing')
 
 
 # LANDING, REGISTER, and LOGIN pages
 @app.route('/landing')
 def landing():
-    return render_template('landing.html')
+    if 'email_address' in session:
+        # User is logged in, redirect to homepage
+        return redirect('/homepage')
+    else:
+        # User is not logged in, render the landing page
+        return render_template('landing.html')
 
 
 @app.route('/register', methods=['POST'])
@@ -28,6 +56,13 @@ def register_route():
     last_name = request.form['last_name']
     email_address = request.form['email_address']
     password = request.form['password']
+
+    # Perform domain validation
+    allowed_domains = ["apc.edu.ph",
+                       "student.apc.edu.ph", "bigbrews", "tester"]
+    domain = email_address.split("@")[-1]
+    if domain not in allowed_domains:
+        return "Invalid email domain. Please use an email address from the allowed domains."
 
     # Call the register_user function from register.py
     result = register_user(first_name, last_name, email_address, password)
@@ -39,30 +74,64 @@ def register_route():
         return 'Error registering user'
 
 
-@app.route('/login', methods=['POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login_route():
-    email_address = request.form['email_address']
-    password = request.form['password']
+    if request.method == 'POST':
+        email_address = request.form['email_address']
+        password = request.form['password']
 
-    # Call the login_user function from login.py
-    result = login_user(email_address, password)
+        # Call the login_user function from login.py
+        result = login_user(email_address, password)
 
-    if result:
-        session['email_address'] = email_address
-        return redirect('/homepage')
+        if result:
+            session['email_address'] = email_address
+            return redirect('/homepage')  # Redirect to /homepage route
+        else:
+            error_message = 'Invalid email/password combination'
+            return render_template('landing.html', error_message=error_message)
     else:
-        return 'Invalid email/password combination'
+        return render_template('landing.html')
+
+
+@app.route('/logout')
+def logout():
+    # Clear the session data
+    session.clear()
+    return redirect('/landing')
 
 
 # HOMEPAGE and SUBMISSION pages
 @app.route('/homepage')
 def homepage():
-    return render_template('homepage.html')
+    if 'email_address' not in session:
+        # User is not logged in, redirect to landing
+        return redirect('/landing')
+
+    # Get the email address from the session
+    email_address = session['email_address']
+
+    # Retrieve the user ID associated with the logged-in email address
+    user_id = get_user_id(email_address)
+
+    if user_id is None:
+        # User ID not found, redirect to landing
+        return redirect('/landing')
+
+    # Retrieve user-specific submissions based on the user ID
+    submissions = get_user_submissions(user_id)
+
+    return render_template('homepage.html', submissions=submissions)
 
 
 @app.route('/submission')
 def submission():
-    return render_template('submission.html')
+    if 'email_address' in session:
+        email_address = session['email_address']
+        user_id = get_user_id(email_address)
+        submissions = get_user_submissions(user_id)
+        return render_template('submission.html', submissions=submissions)
+    else:
+        return redirect('/landing')
 
 
 @app.route('/submit', methods=['POST'])
@@ -89,8 +158,22 @@ def submit_form():
     if 'keywords' in request.form:
         form_data['keywords'] = request.form.getlist('keywords[]')
 
+    if 'email_address' not in session:
+        # User is not logged in, redirect to landing
+        return redirect('/landing')
+
+    # Get the email address from the session
+    email_address = session['email_address']
+
+    # Retrieve the user ID associated with the logged-in email address
+    user_id = get_user_id(email_address)
+
+    if user_id is None:
+        # User ID not found, redirect to landing
+        return redirect('/landing')
+
     # Call submit function to handle MySQL database operations
-    submit.submit_to_database(form_data)
+    submit.submit_to_database(form_data, user_id)
 
     return "Form submitted successfully!"
 
@@ -100,18 +183,6 @@ def submit_form():
 def admin():
     submissions = fetch_submissions()
     return render_template('admin.html', submissions=submissions)
-
-
-@app.route('/update_status', methods=['POST'])
-def update_status():
-    # Get the submission ID and new status from the request JSON data
-    submission_id = request.json['submissionId']
-    new_status = request.json['status']
-
-    # Update the status in the database
-    update_submission_status(submission_id, new_status)
-
-    return jsonify({'message': 'Status updated successfully'})
 
 
 if __name__ == '__main__':
